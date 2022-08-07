87386a2c004d29d8360540f94208c4458733579c
custardcream@kakao.com <custardcream@kakao.com>
2022-08-07 22:11:54 +0900

    Init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+function App() {
+  return <div className="App">테스트!</div>;
+}
+
+export default App;
