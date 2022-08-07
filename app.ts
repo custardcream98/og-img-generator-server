694830ec01deeba487462d1d57cabb4ce5028482
custardcream@kakao.com <custardcream@kakao.com>
2022-08-07 23:45:25 +0900

    Bugfix: Refector

--- app.ts
+++ app.ts
@@ -20,9 +20,9 @@ app.get('/ogimage', async (req, res) => { // Note the async
     }
   });
   const page = await browser.newPage();
-  await page.goto(`${process.env.BASE_URL}?title=${req.query.title}` || `http://localhost:5000/?title=${req.query.title}`);    
+  // await page.goto(`${process.env.BASE_URL}?title=${req.query.title}` || `http://localhost:5000/?title=${req.query.title}`);    
   
-  // await page.goto(path.join("file://",__dirname, "..","..","build", "index.html"));    
+  await page.goto(path.join("file://",__dirname, "..","client","build", "index.html") + `?title=${req.query.title}`,{waitUntil:"load"});    
   const element = await page.$('#root');
   const image = await element!.screenshot({ omitBackground: true });  
   await browser.close(); 
