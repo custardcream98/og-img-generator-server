8c035ede6950a174bc68e4db31b90fdc31512d47
custardcream@kakao.com <custardcream@kakao.com>
2022-08-09 15:52:01 +0900

    Chore: Clean Code

--- api/index.ts
+++ api/index.ts
@@ -46,27 +46,6 @@ ${htmlString}
   const page = await browser.newPage();
   
   await page.setContent(content, { waitUntil: "domcontentloaded" });  
-  // Wait until all images and fonts have loaded
-  // await page.evaluate(async () => {
-  //   const selectors = Array.from(document.querySelectorAll("img"));
-  //   await Promise.all([
-  //     document.fonts.ready,
-  //    ...selectors.map((img) => {
-  //       // Image has already finished loading, let’s see if it worked
-  //       if (img.complete) {
-  //         // Image loaded and has presence
-  //         if (img.naturalHeight !== 0) return;
-  //         // Image failed, so it has no height
-  //         throw new Error("Image failed to load");
-  //       }
-  //       // Image hasn’t loaded yet, added an event listener to know when it does
-  //       return new Promise((resolve, reject) => {
-  //         img.addEventListener("load", resolve);
-  //         img.addEventListener("error", reject);
-  //       });
-  //     }),
-  //   ]);
-  // });
 
   const image = await page.screenshot({ omitBackground: true });  
   await browser.close();
